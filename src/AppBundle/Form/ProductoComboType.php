e173e6fb6e8e613c16528b890eb5e3e082c7c9ba
Matias Solis de la Torre <matias.delatorre89@gmail.com>
2016-06-11 21:23:37 -0300

    Se arregla el formulario de unidad de medida.

--- src/AppBundle/Form/ProductoComboType.php
+++ src/AppBundle/Form/ProductoComboType.php
@@ -2,30 +2,32 @@
 
 namespace AppBundle\Form;
 
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ProductoComboType extends AbstractType
-{
-    /**
-     * @param FormBuilderInterface $builder
-     * @param array $options
-     */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder
-            ->add('producto')
-        ;
-    }
-    
-    /**
-     * @param OptionsResolver $resolver
-     */
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefaults(array(
-            'data_class' => 'AppBundle\Entity\ProductoCombo'
-        ));
-    }
+class ProductoComboType extends AbstractType {
+	/**
+	 * @param FormBuilderInterface $builder
+	 * @param array $options
+	 */
+	public function buildForm( FormBuilderInterface $builder, array $options ) {
+		$builder
+			->add( 'producto',
+				EntityType::class,
+				array(
+					'class' => 'AppBundle\Entity\Producto',
+					'attr'  => array( 'multiple' => false, 'data-widget' => 'select2' )
+				) );
+	}
+
+	/**
+	 * @param OptionsResolver $resolver
+	 */
+	public function configureOptions( OptionsResolver $resolver ) {
+		$resolver->setDefaults( array(
+			'data_class' => 'AppBundle\Entity\ProductoCombo'
+		) );
+	}
 }
